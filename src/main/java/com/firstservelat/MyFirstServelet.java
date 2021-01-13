23467ccd7e454d15a6b4cdf7b950a24918c14ccb
aman007gupta <aman9453122969@gmail.com>
2021-01-13 11:48:51 +0530

    [Aman]Added: code to create servlet

---
+++ src/main/java/com/firstservelat/MyFirstServelet.java
@@ -0,0 +1,19 @@
+package com.firstservelat;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/FirstServlet")
+public class MyFirstServelet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws SecurityException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h3>Hello World Aman My First Servlet!</h3>");
+        out.close();
+    }
+}
